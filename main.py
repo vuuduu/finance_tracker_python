34bac635d88ea9ae6d71a124a9114ce66ae4c396
Vu Nguyen <vunguyen@email.com>
2024-12-10 00:11:49 -0500

    update file location and add in years choices

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from src.display import Display
+import os
+
+def display_available_year(data_path: str) -> int:
+    years = sorted(os.listdir(data_path), reverse=True)
+
+    if len(years) > 0:
+        print('Select the year you want to view your expenses')
+        for i in range(len(years)):
+            print(f"{i+1}. {years[i]}")
+    else:
+        print('You have no available year trackers!')
+    
+    print(f"{len(years) + 1}. Create a new year directory")
+    return len(years) + 1
+
+
+def verify_choices(possible_choices: int, choice: int) -> bool:
+    if possible_choices <= choice <= 1:
+        return True
+    
+    return False
 
 
 if __name__ == '__main__':
-    expense_data_path = 'data/json/expenses.json'
-    income_data_path = 'data/json/incomes.json'
-    payoff_data_path = 'data/json/payoff.json'
-    tracker_data_path = 'data/json/trackers.json'
+    json_data_path = 'data/json'
+    # test_data_path = 'data/csv'
 
+    # Display welcome screen
     print('\n#####################################')
     print('Welcome to Expenses & Incomes Tracker')
     print('#####################################\n')
+
+    # Display available year
+    possible_choices = display_available_year(json_data_path)
+    print(f"\nPosisble choices = {possible_choices}")
