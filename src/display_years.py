201ace4e7e10323d19ab9525243462d277a8ad1c
Vu Nguyen <vunguyen@email.com>
2024-12-11 20:10:44 -0500

    able to get input and display year for input

--- src/display_years.py
+++ src/display_years.py
@@ -1,40 +1,63 @@
 import os
 
+CHOICE = "choice"
+QUIT = "quit"
+
 class DisplayYear:
 
     def __init__(self, base_path: str):
         self.years = self.__get_years(base_path)
 
+
+    # Very if it's either create or retrieve
+    # - Create: 
+    # --- create a new folder with a new year name
+    # --- create 3 new json file (expense, income, payoff)
+    # --- ensure the new year doesn't exist yet
+    # --- return an object {"action": "create", "year": "<year_created>", "data": None}
+    # - Retreive:
+    # --- get a list of data expense, income, & payoff
+    # --- return an object 
+    '''
+        {"class_name": "DisplayYear",
+            "action": "get", 
+            "year": "<year_selected>", 
+            "data": None || 
+                    {"expense": [], 
+                    "income": [], 
+                    "payoff": []}
+        }
+    '''
     def run(self):
         print("\nSelect the possible option(s) below")
+
         while True:
             self.__display_years(self.years)
+
             # Get input
-            # Verify if input is correct 
-            # Very if it's either create or retrieve
-            # - Create: 
-            # --- create a new folder with a new year name
-            # --- create 3 new json file (expense, income, payoff)
-            # --- ensure the new year doesn't exist yet
-            # --- return an object {"action": "create", "year": "<year_created>", "data": None}
-            # - Retreive:
-            # --- get a list of data expense, income, & payoff
-            # --- return an object 
-            '''
-                {"class_name": "DisplayYear",
-                 "action": "get", 
-                 "year": "<year_selected>", 
-                 "data": None || 
-                          {"expense": [], 
-                           "income": [], 
-                           "payoff": []}
-                }
-            '''
+            inputs = self.__get_input(len(self.years))
+            if inputs[QUIT]:
+                print(inputs)
+                break
+            print("\n Must enter a valid choice(s)!")
+            print(inputs)
 
 
-            # choice = input("Enter an input")
-            # if choice == 'q' or self.__verify_choice(int(choice), len(.years) + 1):
-                
+    def __get_input(self, possible_choices: int):
+        choice = 0
+        quit = False
+        try:
+            choice = input("Enter a valid choice (q to quit): ")
+            
+            # Verify the inputs
+            if choice in ['q', 'quit', 'Q']:
+                quit = True
+            else:
+                quit = self.__verify_choice(int(choice), possible_choices)
+        except ValueError:
+            print("\n Choices CANNOT be a non-numeric value")
+
+        return {"choice": choice, "quit": quit}
 
     def __get_years(self, base_path: str) -> list:
         years = []
@@ -50,9 +73,6 @@ class DisplayYear:
         print(f"{len(years)+1}. Create a new year trackers")
 
     def __verify_choice(self, choice: int, possible_choices: int) -> bool:
-        if 1 <= choice <= possible_choices:
+        if 1 <= choice <= possible_choices + 1:
             return True
         return False
-    
-    def __return_object(self, choice):
-        return {}
